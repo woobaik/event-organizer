05b82abdafde6ea6235001601676b4e61122ec67
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-12-18 20:29:31 -0800

    Add semantic-UI

---
+++ src/Components/Layouts/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Navigtaion from "../Navigation/Navigation.jsx"
+
+function App() {
+  return (
+    <div className='App'>
+      <Navigtaion />
+      <div>Grid</div>
+    </div>
+  )
+}
+
+export default App
