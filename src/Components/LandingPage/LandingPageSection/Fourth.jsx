a64d5d08980b32c666d59b34a9e0af178eb2aaf1
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-12-19 20:39:01 -0800

    Finish app page

--- src/Components/LandingPage/LandingPageSection/Fourth.jsx
+++ src/Components/LandingPage/LandingPageSection/Fourth.jsx
@@ -1,7 +1,20 @@
 import React from "react"
+import classes from "./Fourth.module.css"
+import fourthImg from "../../../assets/images/fourth.en.png"
 
 const Fourth = () => {
-  return <div>Fourth</div>
+  return (
+    <div className={classes.Fourth}>
+      <img className={classes.fourthImg} src={fourthImg} />
+      <div className={classes.fourthText}>
+        <div>CREATE</div>
+        <div>Plan Events on the fly</div>
+        <div>
+          Manage your Meetup group and plan your next event—all from your phone.
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Fourth
