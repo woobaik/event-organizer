a0c3915e2f356fc4daee87fe63e842381d68ff6b
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-12-19 19:58:04 -0800

    Finish third

--- src/Components/LandingPage/LandingPageSection/Second.jsx
+++ src/Components/LandingPage/LandingPageSection/Second.jsx
@@ -9,6 +9,7 @@ const Second = () => {
         <img className={classes.secondImg} src={SecondImg} />
       </section>
       <section className={classes.textArea}>
+        <h4>DISCOVER</h4>
         <h1 className={classes.header}>Find what inspires you</h1>
         <p>Find groups in your area that are doing things that interest you</p>
       </section>
--- src/Components/LandingPage/LandingPageSection/Third.jsx
+++ src/Components/LandingPage/LandingPageSection/Third.jsx
@@ -1,7 +1,20 @@
 import React from "react"
+import ThirdImg from "../../../assets/images/third.en.png"
+import classes from "./Third.module.css"
 
 const Third = () => {
-  return <div>Third</div>
+  return (
+    <div className={classes.Third}>
+      <div className={classes.thirdText}>
+        <div>connect</div>
+        <div>Make real connection</div>
+        <div>
+          meet people who love what you love, and stay in touch after the vent
+        </div>
+      </div>
+      <img className={classes.thirdImg} src={ThirdImg} />
+    </div>
+  )
 }
 
 export default Third
