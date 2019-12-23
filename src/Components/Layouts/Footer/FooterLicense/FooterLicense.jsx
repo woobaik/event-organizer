487d3966f88810c823a7e10e42d4f2e92726c402
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-12-22 22:56:55 -0800

    Add text to footer license

--- src/Components/Layouts/Footer/FooterLicense/FooterLicense.jsx
+++ src/Components/Layouts/Footer/FooterLicense/FooterLicense.jsx
@@ -1,7 +1,13 @@
 import React from "react"
 import classes from "./FooterLicense.module.css"
 const FooterLicense = () => {
-  return <div> Footer License</div>
+  return (
+    <div>
+      {" "}
+      © Meetup 2019Meetup is a wholly owned subsidiary of WeWork Companies Inc.
+      Terms of ServicePrivacy PolicyCookie Policy
+    </div>
+  )
 }
 
 export default FooterLicense
