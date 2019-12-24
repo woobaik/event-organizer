729da1c6108f8bcebee155afaddf820b71b73368
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-12-23 23:19:57 -0800

    mock up footer naviation

--- src/Components/Layouts/Footer/FooterNavigation/FooterNavigation.jsx
+++ src/Components/Layouts/Footer/FooterNavigation/FooterNavigation.jsx
@@ -2,7 +2,14 @@ import React from "react"
 import classes from "./FooterNavigation.module.css"
 
 const FooterNavigation = () => {
-  return <div>FooterNavigation</div>
+  return (
+    <div>
+      <div>Your Account Sign up Log in Help</div>
+      <div>Discover Groups Calendar Topics Cities</div>
+      <div>Meetup About Meetup Pro Careers Apps API Accessibility</div>
+      <div>Follow us</div>
+    </div>
+  )
 }
 
 export default FooterNavigation
