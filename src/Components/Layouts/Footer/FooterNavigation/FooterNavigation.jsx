73cfdf77bd111af12ef7ae863f8de32f54f20c94
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-12-25 17:39:38 -0800

    remove unnecessary task

--- src/Components/Layouts/Footer/FooterNavigation/FooterNavigation.jsx
+++ src/Components/Layouts/Footer/FooterNavigation/FooterNavigation.jsx
@@ -8,7 +8,6 @@ const FooterNavigation = () => {
       <div>Your Account Sign up Log in Help</div>
       <div>Discover Groups Calendar Topics Cities</div>
       <div>Meetup About Meetup Pro Careers Apps API Accessibility</div>
-      <div>Follow us</div>
     </div>
   )
 }
